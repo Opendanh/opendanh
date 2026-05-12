ad09d0efacfa3c67f06a74ae26f338837c6fcc11
danhcan2025 <danhcan2025@gmail.com>
2026-05-12 07:36:30 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,32 @@
 
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
